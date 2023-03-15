5e7db00a89d5e20bebbd1345af88571af89c77d5
anish <anish@noreply.com>
2023-03-15 12:21:38 +0530

    Write file asych

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,12 @@ pub async fn download_file(url: &Url) -> Result<(), Box<dyn std::error::Error>> 
     let mut response = reqwest::get(url.as_ref()).await?;
     let start_time = Instant::now();
     let file_name = url.path_segments().and_then(|segments| segments.last()).unwrap_or("file.bin");
-    let mut file = File::create(file_name)?;
+    let mut file = tokio::fs::File::create(file_name).await?;
     let mut downloaded_size = 0;
-    //let mut _buf = [0; 8192];
     while let Some(chunk) = response.chunk().await? {
         let len = chunk.len();
         downloaded_size += len;
-        file.write_all(&chunk);
+        file.write_all(&chunk).await?;
         let elapsed_time = start_time.elapsed();
         let elapsed_seconds = elapsed_time.as_secs_f64();
         let download_speed = downloaded_size as f64 / elapsed_seconds / 1_000_000.0;
@@ -40,6 +39,7 @@ pub async fn download_file(url: &Url) -> Result<(), Box<dyn std::error::Error>> 
     Ok(())
 }
 
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = match env::args().nth(1) {
@@ -49,31 +49,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     download_file(&url).await?;
     Ok(())
 }
-
-
-// fn main() -> Result<(), Box<dyn Error>> {
-//     let url = match env::args().nth(1) {
-//         Some(url_str) => Url::parse(&url_str)?,
-//         None => Url::parse("https://speed.hetzner.de/100MB.bin")?,
-//     };
-//     download_file(&url)?;
-//     Ok(())
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//
-//     #[test]
-//     fn test_download_file() {
-//         let url = Url::parse("https://speed.hetzner.de/100MB.bin").unwrap();
-//         download_file(&url).unwrap();
-//         let file = File::open("100MB.bin").unwrap();
-//         let file_size = file.metadata().unwrap().len();
-//         assert_eq!(file_size, 104_857_600);
-//         std::fs::remove_file("100MB.bin").unwrap();
-//     }
-// }
 
 #[cfg(test)]
 mod tests {
